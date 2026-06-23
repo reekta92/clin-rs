ac473848be369e46ef3557df6a737623f31e0917
Mehmet Dağ <mdag.92988@protonmail.com>
2026-06-23 04:31:05 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "clin";
-          version = "0.9.0-beta.4"; # This will be updated by the release workflow
+          version = "0.8.26"; # This will be updated by the release workflow
 
           src = ./.;
 
