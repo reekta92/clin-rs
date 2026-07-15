e4cf7462c21794d4a66142fb026abc7f2e456d25
reekta92 <mdag.92988@protonmail.com>
2026-07-15 11:17:52 +0300

    chore: cleanup project dir

--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import re
-
-content = open("src/events/popup_mouse.rs").read()
-# find Template branch
-match = re.search(r"Template\(mut p\).*?(?=\s*// === Group C4)", content, re.DOTALL)
-if match:
-    print(match.group(0))
