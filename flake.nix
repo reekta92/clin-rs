21d299276e7279ec99f0325f09bb545ab0d44dfb
reekta92 <mdag.92988@protonmail.com>
2026-08-03 15:48:24 +0300

    chore: revert version to 0.9.9 to allow 0.9.10 release

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "clin";
-          version = "0.10.0-beta.0"; # This will be updated by the release workflow
+          version = "0.9.9"; # This will be updated by the release workflow
           doCheck = false;
 
           src = ./.;
