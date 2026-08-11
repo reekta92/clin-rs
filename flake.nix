c359a0ec181a3d0d4cf24d68578b42249f9944a0
Mehmet Dağ <mdag.92988@protonmail.com>
2026-08-11 19:07:07 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in
