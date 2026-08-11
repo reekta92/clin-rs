43018a19b6b78493f44f60c796927084b0980f7e
reekta92 <mdag.92988@protonmail.com>
2026-08-11 18:45:02 +0300

    fix: restore x86_64-darwin support

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
+        "x86_64-darwin"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in
